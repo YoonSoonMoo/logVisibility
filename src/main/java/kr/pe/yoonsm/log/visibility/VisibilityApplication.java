522ae37241ae422f597e3d7f007180a08a974336
soonmoo <ysmb482@gmail.com>
2023-02-03 18:54:18 +0900

    init project

---
+++ src/main/java/kr/pe/yoonsm/log/visibility/VisibilityApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.yoonsm.log.visibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisibilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/pe/yoonsm/log/visibility/VisibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.yoonsm.log.visibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
