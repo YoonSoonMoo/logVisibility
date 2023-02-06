5b8a639097227fab255287656a5c4d9d6ce2e5c0
soonmoo <ysmb482@gmail.com>
2023-02-06 16:16:25 +0900

    logback설정 추가

---
+++ src/main/java/kr/pe/yoonsm/log/visibility/controller/LogVisibilityController.java
@@ -0,0 +1,26 @@
+package kr.pe.yoonsm.log.visibility.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yoonsm@daou.co.kr on 2023-02-06
+ */
+@RestController
+public class LogVisibilityController {
+
+    private static final Logger logger
+            = LoggerFactory.getLogger(LogVisibilityController.class);
+
+    @RequestMapping("/")
+    public String printLog() {
+        logger.debug("[REDIS] Set redis,LOCATION:NG0000-LG0000-BB0000-BP0002-RR0000, RedisRiderInfo(loginId=jy_rider)");
+        logger.info("ACTIVERIDER_STAT@ NG0000-LG0000-BB0000-BP0002-RR0000, 정연지점, 1");
+        logger.warn("Exception occurred, retry count 1");
+        logger.error("Rider unlock fail... action:RIDER_POINT_UNLOCK,orderNo:221128170030NG0001-BP0000-SS0019CMAuDu");
+
+        return "콘솔에서 로그를 확인해 보세요.";
+    }
+}
---
+++ src/main/java/kr/pe/yoonsm/log/visibility/filter/MDCLogFilter.java
@@ -0,0 +1,41 @@
+package kr.pe.yoonsm.log.visibility.filter;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by yoonsm@daou.co.kr on 2023-02-06
+ */
+@Component
+public class MDCLogFilter extends OncePerRequestFilter {
+
+    final String REMOTE_IP = "175.119.111.123";
+    final String SESSION = "OF30E51ABDFG17DF77EE8D73849583478DA";
+    final String ACCOUNT_ID ="yoonsm";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+
+        try {
+            MDC.put("remoteIp", REMOTE_IP);
+            MDC.put("sessionId", SESSION);
+            MDC.put("accountId",ACCOUNT_ID);
+
+            filterChain.doFilter(request,response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        } finally {
+            MDC.clear();
+        }
+
+    }
+}
